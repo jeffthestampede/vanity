c54d0a94fe46d29b0926441b145350fd6607e8bf
Jeffrey Kao <jeffthestampede@gmail.com>
2013-06-11 12:59:40 -0700

    Link picture attribute on the Item to the PictureUploader

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,4 +2,6 @@ class Item < ActiveRecord::Base
   attr_accessible :brand, :category, :description, :exp_date, :name, :picture, :purchase_date, :user_id
 
   belongs_to :user
+
+  mount_uploader :picture, PictureUploader
 end
