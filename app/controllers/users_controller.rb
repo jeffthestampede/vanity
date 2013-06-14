43c42efe36ace6abd7ee64c6f81a39905f8aefe2
Jeffrey Kao <jeffthestampede@gmail.com>
2013-06-14 00:22:07 -0700

    added pinterest style view for collection

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,4 +10,14 @@ class UsersController < ApplicationController
     end
   end
 
+  def list
+    @user = User.find(params[:id])
+    @items = @user.items
+
+    respond_to do |format|
+      format.html # show.html.erb
+      format.xml  { render :xml => @user }
+    end
+  end
+
 end
